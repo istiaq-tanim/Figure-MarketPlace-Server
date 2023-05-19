b951b1b5f8cb554bedf0ca50e7d3183e700b9797
istiaq-tanim <istiaq.tanim17@gmail.com>
2023-05-19 08:46:16 +0600

    single toy api created

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
-const app=express();
+const app = express();
 
-const port=process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
@@ -27,6 +27,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const toysCollections = client.db("actionDB").collection("toys");
+
+    app.get("/toys/:category", async (req, res) => {
+      const category = req.params.category;
+      console.log(category)
+      if (category === "Marvel" || category === "DC" || category === "Transformers") {
+        const result = await toysCollections.find({subCategory:category}).limit(3).toArray();
+        return res.send(result)
+      }
+
+    })
+
+    app.get("/toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollections.findOne(query);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -39,11 +58,10 @@ run().catch(console.dir);
 
 
 
-app.get("/",(req,res)=>{
-    res.send("Toy Market is Running")
+app.get("/", (req, res) => {
+  res.send("Toy Market is Running")
 })
 
-app.listen(port,()=>
-{
-    console.log(`Toy Market is running on port${port}`)
+app.listen(port, () => {
+  console.log(`Toy Market is running on port${port}`)
 })
